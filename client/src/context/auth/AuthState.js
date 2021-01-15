ad9e2ef443b676de49d3628c269619664612fc38
Scott Snodgrass <github@ssnodgrass.com>
2021-01-15 16:28:40 -0500

    Logout & Navbar

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -91,6 +91,7 @@ const AuthState = (props) => {
   };
 
   // Logout
+  const logout = () => dispatch({ type: LOGOUT });
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
@@ -105,6 +106,7 @@ const AuthState = (props) => {
         error: state.error,
         register,
         login,
+        logout,
         clearErrors,
         loadUser,
       }}
