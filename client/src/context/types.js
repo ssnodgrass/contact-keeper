8766024ab895ef788dd4d42f0d194272f35ce82c
Scott Snodgrass <github@ssnodgrass.com>
2021-01-15 15:20:29 -0500

    Alert Context, State & Component

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -6,8 +6,6 @@ export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 export const UPDATE_CONTACT = 'UPDATE_CONTACT';
 export const FILTER_CONTACTS = 'FILTER_CONTACTS';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
-export const SET_ALERT = 'SET_ALERT';
-export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 
 // Auth
@@ -18,3 +16,7 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCES';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+// Alert
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
